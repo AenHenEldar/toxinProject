aed07904ee846d3518855da1260a65420a8cd9e1
Eldar <eldar.hidirow@gmail.com>
2020-02-06 13:44:01 +0300

    Шото мучу с вебпаком

---
+++ webpack-toxin/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader?url=false', 'sass-loader']
+      })
+    }, {
+      test: /\.pug$/,
+      use: ['pug-loader']
+    }, {
+      test: /\.svg$/,
+      loader: 'svg-inline-loader'
+    }, {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true, // webpack@1.x
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
+    }, {
+      test: /\.(png|jpg|gif)$/i,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      }, ],
+    }, ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+  ]
+};
